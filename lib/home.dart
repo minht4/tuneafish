f32ebfc4e287f849949b4c0e8b9ff293069464ac
MythosZXA <ubw.zeroxa@gmail.com>
2021-10-04 00:32:16 -0500

    yo mom

--- lib/home.dart
+++ lib/home.dart
@@ -23,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
       children: <Widget>[
         const SizedBox(height: 10),
         const Text(
-          "Dashboard",
+          "Dashboarda",
           style: TextStyle(
             fontSize: 30,
 
