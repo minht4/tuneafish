e7d5b0b561350950f3f3b5ab1b7b0a679a1f712c
Minh Tang <minht4@outlook.com>
2021-10-04 00:00:42 -0500

    DD

--- lib/home.dart
+++ lib/home.dart
@@ -34,7 +34,7 @@ class _HomeScreenState extends State<HomeScreen> {
           child: Row(
             children: [
               const Text(
-                "Temperature: ",
+                "Temperature:d ",
                 style: TextStyle(
                   fontSize: 20,
                 ),
