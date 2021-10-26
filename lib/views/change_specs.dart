3723d2f29d37eb3e3c4da805d0b50af51961b171
Toan Tran <73968515+MythosZXA@users.noreply.github.com>
2021-10-25 21:26:00 -0500

    Add files via upload

---
+++ lib/views/change_specs.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class SpecsScreen extends StatefulWidget {
+  const SpecsScreen({Key? key}) : super(key: key);
+
+  @override
+  _SpecsScreenState createState() => _SpecsScreenState();
+}
+
+class _SpecsScreenState extends State<SpecsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text(''),),
+      body: Center(
+        child: Column(
+          children:  const <Widget>[
+            SizedBox(height: 50),
+            Text('Change tank size here'),
+            SizedBox(height: 20),
+            Text('Change # of fishes here'),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/views/settings.dart
+++ lib/views/settings.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:getxtest/views/specs.dart';
+
+import 'change_specs.dart';
 
 class SettingsScreen extends StatefulWidget {
   const SettingsScreen({Key? key}) : super(key: key);
@@ -13,22 +13,26 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Column(
-        children: <Widget>[
-          const SizedBox(height: 20),
-          const Text(
-            "Settings",
-            style: TextStyle(
-              fontSize: 30,
-            ),
+      children: <Widget>[
+        const SizedBox(height: 10),
+        const Text(
+          "Settings",
+          style: TextStyle(
+            fontSize: 30,
           ),
-          const SizedBox(height: 20),
-          FloatingActionButton.extended(
-              onPressed: () {
-                Get.to(SpecsScreen());
-              },
-              label: const Text('Change Tank Specifications')
-          )
-        ]
+        ),
+        const SizedBox(height: 20),
+        FloatingActionButton.extended(
+            onPressed: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SpecsScreen()),
+              );
+            },
+            label: const Text('Change Tank Specifications')
+        )
+    ]
     );
+
   }
 }
