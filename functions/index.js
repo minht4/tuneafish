f38cd552b95a128599e68a94b2597bd03d70d7ae
MD <mdimtiazmh@live.com>
2022-04-08 02:12:23 -0500

    cloud functions#2

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,17 @@ const admin = require('firebase-admin');
 admin.initializeApp(functions.config().firebase);
 
 exports.notifyTemp = functions.firestore.document('specs/temperature').onUpdate(async (event) => {
-    const temperature = event.after.get('temp');
+    const temp = event.after.get('temp');
+    const pretemp = event.before.get('temp');
+
    
     let userDoc = await admin.firestore().doc(`UserTokens/Fcm-Token`).get();
     let fcmToken = userDoc.get('token');
-    if(temperature>40 || temperature<5){
+    if((pretemp <= 83 && temp >= 83) || (pretemp >= 71 && temp <= 71)){
     var message = {
         notification: {
-            title: "Temperature fluctuation",
-            body: "Current tank temperature is at an extreme: "+temperature,
+            title: "Temperature Fluctuation Emergency!",
+            body: "Current tank temperature is at an extreme: "+temp+"F",
         
         },
         token: fcmToken,
@@ -20,15 +22,29 @@ exports.notifyTemp = functions.firestore.document('specs/temperature').onUpdate(
 
     let response = await admin.messaging().send(message);
     console.log(response);
-    }else return 0;
+    }
+    else if((pretemp >= 77 && (temp >= 81 && temp < 83)) || (pretemp <= 77 && (temp <= 73 && temp > 71))){
+        var message = {
+            notification: {
+                title: "Temperature Fluctuation",
+                body: "Please adjust the tank temperature\nCurrent tank temperature: "+temp+"F",
+            
+            },
+            token: fcmToken,
+        }
+    
+        let response = await admin.messaging().send(message);
+        console.log(response);
+        }else return 0;
 });
 
 exports.notifyWaterLevel = functions.firestore.document('specs/waterLevel').onUpdate(async (event) => {
     const waterlvl = event.after.get('level');
-   
+    const prewaterlvl = event.before.get('level');
+
     let userDoc = await admin.firestore().doc(`UserTokens/Fcm-Token`).get();
     let fcmToken = userDoc.get('token');
-    if(waterlvl<=  10){
+    if(((prewaterlvl  >= 1) && (waterlvl <=  1))) {
     var message = {
         notification: {
             title: "Tank Water Level fluctuation",
